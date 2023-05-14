4430414bf3021466a2b8311855a4616e3d95f950
Leandro Albero <leandro80gb@gmail.com>
2023-05-14 23:25:46 +0200

    Add information about device

--- src/handler.py
+++ src/handler.py
@@ -6,15 +6,18 @@ from easynmt import EasyNMT  # type: ignore
 model = EasyNMT("opus-mt")
 
 
-def handler(event: Any) -> str:
+def handler(event: Any) -> dict[str, str]:
     src_lang = event["input"]["src_lang"] if "src_lang" in event["input"] else "es"
     target_lang = (
         event["input"]["target_lang"] if "target_lang" in event["input"] else "en"
     )
-
-    return model.translate(
+    translated = model.translate(
         event["input"]["input_text"], target_lang=target_lang, source_lang=src_lang
     )
+    return {
+        "result": translated,
+        "debug": model.device,
+    }
 
 
 runpod.serverless.start({"handler": handler})
