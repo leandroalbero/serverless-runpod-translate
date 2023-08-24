5c155cba19c57f834627b71b810552671230e427
Leandro <leandro80gb@gmail.com>
2023-08-24 17:01:51 +0200

    preload language detection model

--- src/cache_models.py
+++ src/cache_models.py
@@ -9,6 +9,7 @@ def get_models() -> EasyNMT:
     model = EasyNMT("m2m_100_418m")
 
     # Translate so it downloads the models
+    model.translate("autodetectar", target_lang="en")
     model.translate("a", source_lang="es", target_lang="en")
     model.translate("a", source_lang="fr", target_lang="en")
     model.translate("a", source_lang="pt", target_lang="en")
